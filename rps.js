dd4f72908ce087cdd7e180ab69eb13c80ed4677d
Michael Austin <MAustin428@gmail.com>
2022-11-30 22:42:34 -0500

    Create rps.js
    Copied from file. Originally written on 28 Feb 2013

---
+++ rps.js
@@ -0,0 +1,9 @@
+var comp = (Math.floor(Math.random()*3)+2)%3;
+var player = Math.ceil(prompt("rock, paper, scissors?").length/2);
+if (comp === player%3){
+ console.log("Draw!");
+}else if((player - 1)%3 === comp){
+ console.log("Win!");}
+else{
+ console.log("Lose!");
+}
