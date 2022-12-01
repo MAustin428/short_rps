e1a8279701caa7a823f9fbf700a2a1469e3d0787
Michael Austin <MAustin428@gmail.com>
2022-11-30 22:48:15 -0500

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,9 +1,8 @@
 var comp = (Math.floor(Math.random()*3)+2)%3;
 var player = Math.ceil(prompt("rock, paper, scissors?").length/2);
-if (comp === player%3){
+if (comp === player%3)
  console.log("Draw!");
-}else if((player - 1)%3 === comp){
- console.log("Win!");}
-else{
+else if((player - 1)%3 === comp)
+ console.log("Win!");
+else
  console.log("Lose!");
-}
